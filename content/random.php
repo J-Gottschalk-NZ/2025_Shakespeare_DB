aff35ea8de61425a3b15d8dee2e680906c0a5a1a
J-Gottschalk-NZ <49209767+J-Gottschalk-NZ@users.noreply.github.com>
2025-11-04 12:04:32 +1300

    Updated featured / random (big cards, no links)

--- content/random.php
+++ content/random.php
@@ -10,38 +10,84 @@
     list($find_query, $find_count) = get_query($dbconnect, $sql_condition);
 
 
-    ?>
-    
-    <h2>Random</h2>
-
-    <?php
     if($find_count > 0)
 
     {
     ?>
 
-        <h2><?php echo $heading; ?></h2>
+        <h2 class="search-heading"><?php echo $heading; ?></h2>
 
-    <?php
+
+
+
+    <div class='all-cards'>
+
+
+        <?php
 while($find_rs = mysqli_fetch_assoc($find_query)) {
+
+
+    // Search term / Text setup (retrieve text and save as variables for easy reference)
+    // This is mostly to make searching / clickable links easy
+    $character = $find_rs['Character_Name'];
+    $play = $find_rs['Play'];
+    $category = $find_rs['Play_Cat'];
+    $gender = $find_rs['M_or_F'];
+    $role = $find_rs['Role'];
+    $alignment = $find_rs['Alignment'];   
+    $action = $find_rs['Action'];    
+    $method = $find_rs['Method'];    
+
+    $triat1 = $find_rs['Trait1'];
+    $triat2 = $find_rs['Trait2'];
+    $triat3 = $find_rs['Trait3'];
+
+    // Image and icon set up...
+    $category_icon = "images/icons/".$find_rs['Category_Icon'];
+    $gender_icon = "images/icons/".$find_rs['Gender_Icon'];
+    $role_icon = "images/icons/".$find_rs['Role_Icon'];
+    $alignment_icon = "images/icons/".$find_rs['Moral_Icon'];
+    $action_icon = "images/icons/".$find_rs['Action_Icon'];
+    $method_icon = "images/icons/".$find_rs['Method_Icon'];
 
     ?>
 
-    <p><b>Character: </b> <?php echo $find_rs['Character_Name']; ?><br>
-    <b>Play: </b><?php echo $find_rs['Play']; ?><br>
-    <b>Play Category: </b><?php echo $find_rs['Play_Cat']; ?><br>
-    <b>Role: </b><?php echo $find_rs['Role']; ?><br>
-    <b>Gender: </b><?php echo $find_rs['M_or_F']; ?><br>
-    <b>Moral Alignment: </b><?php echo $find_rs['Alignment']; ?><br>
-    <b>Trait 1: </b><?php echo $find_rs['Trait1']; ?><br>
-    <b>Trait 2: </b><?php echo $find_rs['Trait2']; ?><br>
-    <b>Trait 3: </b><?php echo $find_rs['Trait3']; ?><br>
-    <b>COD Action: </b><?php echo $find_rs['Action']; ?><br>
-    <b>COD Method: </b><?php echo $find_rs['Method']; ?><br>
-    <b>Description: </b><?php echo $find_rs['Description']; ?><br>
-
 
 </p>
+
+<div class="char-details">
+
+    <div class="character-name"><?php echo $character; ?></div>
+
+    <div class="play-title">
+        <a title="<?php echo $category; ?>" class="category" href="#"><img src="<?php echo $category_icon; ?>" alt="<?php echo $category; ?>"></a>
+        <a class="play" href="#"><?php echo $play; ?></a>
+    </div>
+
+    <div class="icon-row">
+            <a title="<?php echo $gender; ?>" href="#"><img src="<?php echo $gender_icon; ?>" alt="<?php echo $gender; ?>" ></a>
+            <a title="<?php echo $role; ?>" href="#"><img src="<?php echo $role_icon; ?>" alt="<?php echo $role_icon; ?>"></a>
+            <a title="<?php echo $alignment_icon; ?>" href="#"><img src="<?php echo $alignment_icon; ?>" alt="<?php echo $alignment?>"></a>            
+            <a title="<?php echo $action; ?>" href="#"><img src="<?php echo $action_icon; ?>" alt="<?php echo $action; ?>"></a>
+            <a title="<?php echo $method; ?>" href="#"><img src="<?php echo $method_icon; ?>" alt="<?php echo $method; ?>"></a>            
+    
+    </div> <!-- / icon row -->
+
+    <div class="description">
+    <?php echo $find_rs['Description']; ?>
+    </div>
+
+    <div class="trait-tags">
+        <a href="#" class="trait"><?php echo $triat1; ?></a>
+        <a href="#" class="trait"><?php echo $triat2; ?></a>
+        <a href="#" class="trait"><?php echo $triat3; ?></a>
+
+    </div>  <!-- / trait tags -->
+
+    </div>  <!-- / card container -->
+
+
+
 
 
     <?php
@@ -51,42 +97,7 @@ while($find_rs = mysqli_fetch_assoc($find_query)) {
 ?>
 
 
-    <div class='all-cards'>
-
-
-    <div class="char-details">
-
-    <div class="character-name">Romeo</div>
-
-    <div class="play-title">
-        <a title="Tragedy" class="category" href="#"><img src="images/icons/cat_tragedy.png" alt="tragedy"></a>
-        <a class="play" href="#">Romeo and Juliet</a>
-    </div>
-
-    <div class="icon-row">
-            <a title="male" href="#"><img src="images/icons/gender_male_icon.png" alt="Male" ></a>
-            <a title="major role" href="#"><img src="images/icons/major.png" alt="Major Role"></a>
-            <a title="hero" href="#"><img src="images/icons/Moral_Hero.png" alt="Hero"></a>            
-            <a title="Cause of Death - Suicide" href="#"><img src="images/icons/COD_suicide.png" alt="Cause of Death - Suicide"></a>
-            <a title="Method of Death - Poison" href="#"><img src="images/icons/COD_poison.png" alt="Method of Death - Poison"></a>            
-    
-    </div> <!-- / icon row -->
-
-    <div class="description">
-    A young Montague deeply in love with Juliet; his impulsive actions lead to tragic consquences.
-    </div>
-
-    <div class="trait-tags">
-        <a href="#" class="trait">Passionate</a>
-        <a href="#" class="trait">Impulsive</a>
-        <a href="#" class="trait">Romantic</a>
-
-    </div>  <!-- / trait tags -->
-
-    </div>  <!-- / card container -->
-
-
-</div>  <!-- / all-cards -->
+    </div>  <!-- / all-cards -->
 
 <?php
 
